244e612ef0d2326cee8fbd9437d4ba199a973419
pashkami <pavel.tazetdinov@yandex.ru>
2025-01-08 02:20:49 +0300

    test agaim

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*',).split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
 STATIC_ROOT = "/app/collected_static"
 
