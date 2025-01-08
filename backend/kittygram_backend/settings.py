93c470f712e0ee1eff00432a9ce92c039543bc79
pashkami <pavel.tazetdinov@yandex.ru>
2025-01-08 20:37:01 +0300

    static_changes1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = '/backend_static/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
