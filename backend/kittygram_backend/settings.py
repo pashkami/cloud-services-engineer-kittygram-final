20cdf074ef98a2b8286723221d1ae8b3a3aa26b6
pashkami <pavel.tazetdinov@yandex.ru>
2025-01-09 11:11:49 +0300

    test db6

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
         'NAME': 'postgres',  # Имя базы данных
         'USER': 'postgres',  # Имя пользователя
         'PASSWORD': 'postgres',  # Пароль пользователя
-        'HOST': 'pgdb',  # Наименование контейнера для базы данных в Docker Compose
+        'HOST': 'kittygram_postgres-l',  # Наименование контейнера для базы данных в Docker Compose
         'PORT': '5432',   # Порт базы данных
     }
 }
