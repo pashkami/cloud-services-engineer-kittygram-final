164c686db60fc749c87702738c8b1fa841ac2b08
pashkami <pavel.tazetdinov@yandex.ru>
2025-01-11 16:26:06 +0300

    review1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = True
 
@@ -94,7 +97,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = "/backend_static/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
