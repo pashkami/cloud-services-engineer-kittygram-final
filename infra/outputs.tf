3fa2fe6a4ed0c8c68f816018be7e9d24cae7e75a
pashkami <pavel.tazetdinov@yandex.ru>
2025-05-20 23:35:19 +0300

    erroe

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
-output "VM IP" {
+output "VM_IP" {
   description = "Вывод публичного IP виртуальной машины"
   value       = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
   sensitive   = false
