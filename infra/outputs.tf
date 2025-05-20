fd52297933e69bdfbc2c2cecf051fa6ed96226a4
pashkami <pavel.tazetdinov@yandex.ru>
2025-05-20 23:58:14 +0300

    outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
-  description = "Вывод публичного IP виртуальной машины"
-  value       = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
+  description = "Public IP address of the VM"
+  value       = try(yandex_compute_instance.vm-1.network_interface[0].nat_ip_address, "")
 }
