d7a9223ed213f1d54c9f936cde521b7c74ed2d7a
pashkami <pavel.tazetdinov@yandex.ru>
2025-05-21 15:09:17 +0300

    docker

--- infra/main.tf
+++ infra/main.tf
@@ -118,18 +118,6 @@ resource "yandex_compute_instance" "vm-1" {
         - ca-certificates
         - curl
         - software-properties-common
-
-      runcmd:
-        - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        - echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        - sudo apt-get update -y
-        - sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-        - sudo usermod -aG docker devuser  # Добавляем пользователя в группу docker
-        - sudo systemctl enable docker
-
-      # Установка Docker Compose
-        - sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        - sudo chmod +x /usr/local/bin/docker-compose
       EOT
   }
 }
